54b67bbcc9090a296ac2c8ce3333f22f1fdb6428
masumkhan081 <masumkhan081@gmail.com>
2025-01-13 00:05:03 +0600

    project tabs added

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import Project from "../components/projects/project";
 // import { animated, useSpring } from "@react-spring/web";
 import { projectList } from "../data/projects/projects.data";
+import Button from "../components/common/Button";
 //
 export default function page() {
   // useEffect(() => {
@@ -20,19 +21,31 @@ export default function page() {
   // });
   //
   return (
-    <div
-      // style={{ ...springs }}
-      className=" w-full h-auto sm:py-[50px] md:py-[65px] py-[45px] sm:px-5 flex  md:flex-row flex-col flex-wrap bg-acquamarine justify-around gap-y-[45px]"
-    >
 
-      {projectList.map((project, i) => (
-        <Project
-          key={i}
-          name={project.name}
-          summary={project.summary}
-          versions={project.versions}
-        />
-      ))}
+
+    <div className="brdr flex flex-col gap-3 ">
+
+      <div className="flex justify-center">
+        <Button txt={"Live "} />
+        <Button txt={"Back-end only"} />
+        <Button txt={"Front-end Only"} /></div>
+
+      <div
+        // style={{ ...springs }}
+        className="w-full h-auto sm:py-[50px] md:py-[65px] py-[45px] sm:px-5 flex  md:flex-row flex-col flex-wrap bg-acquamarine justify-around gap-y-[45px]"
+      >
+
+        {projectList.map((project, i) => (
+          <Project
+            key={i}
+            name={project.name}
+            summary={project.summary}
+            versions={project.versions}
+          />
+        ))}
+      </div>
+
+
     </div>
   );
 }
